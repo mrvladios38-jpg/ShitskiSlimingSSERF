ec24d9c05c09b64d960b9b7d3e61be5573c9da6d
mrvladios38-jpg <mrvladios38@gmail.com>
2025-10-02 22:03:56 +0300

    Create HashJava.java

---
+++ HashJava.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+HashMap<String, Integer> leight = new HashMap<>();
+leight.put("giraffe", 450);
+leight.put("fox", 40);
+leight.put("mouse", 15);
+System.out.println(leight.get("giraffe"));
+leight.remove("fox");
